706e210469ea1945a509258349c0f561cf37d455
ricsarda <145287890+ricsarda@users.noreply.github.com>
2024-12-31 11:43:28 +0100

    Update App_online_con_streamlit.py

--- App_online_con_streamlit.py
+++ App_online_con_streamlit.py
@@ -30,14 +30,14 @@ def load_and_execute_script(script_name, files):
         spec.loader.exec_module(module)
         
         # Convertir archivos subidos a buffers y reiniciar el puntero
-        processed_files = {}
-        for key, buffer in processed_files.items():
-            st.write(f"{key}: {type(buffer)}")
-
+        processed_files = {}        
         for key, file in files.items():
             buffer = BytesIO(file.read())
             buffer.seek(0)  # Reinicia el puntero al inicio del buffer
             processed_files[key] = buffer
+
+        for key, buffer in processed_files.items():
+            st.write(f"{key}: {type(buffer)}")
 
         # Llama a la función principal del script con los archivos procesados
         result = module.main(processed_files)
