3087ad77d8e89b417b8166c298127f84ab6aeb51
Ricardo Sarda <145287890+ricsarda@users.noreply.github.com>
2025-01-01 19:18:30 +0100

    Update Credit stock.py

--- scripts/Credit stock.py
+++ scripts/Credit stock.py
@@ -377,7 +377,7 @@ def main(files):
            writer.save()
               
         output.seek(0)  # Reiniciar el puntero del buffer
-    return output  # Devuelve el archivo generado como BytesIO
+        return output  # Devuelve el archivo generado como BytesIO
 
     except Exception as e:
         raise RuntimeError(f"Error al procesar el script: {str(e)}")
