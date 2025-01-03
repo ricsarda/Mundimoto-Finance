d8cc250c41295a8df4694ae99448f912d6b96f37
Ricardo Sarda <145287890+ricsarda@users.noreply.github.com>
2025-01-03 17:35:48 +0100

    Update Credit stock.py

--- scripts/Credit stock.py
+++ scripts/Credit stock.py
@@ -512,8 +512,7 @@ def main(files, excel, month=None, year=None):
             motosparsofinco.to_excel(writer, sheet_name='Motos Sofinco', index=False)
             CreditStock.to_excel(writer, sheet_name='Control', index=False)
 
-        
-
+        excel.seek(0)  # Reiniciar el puntero del buffer
         return excel.getvalue()  # Devuelve el archivo generado como BytesIO
 
     except Exception as e:
