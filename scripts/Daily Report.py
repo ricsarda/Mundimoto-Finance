a39ae0da1509165db2c21c4fe6efcbc86da6aebd
Ricardo Sarda <145287890+ricsarda@users.noreply.github.com>
2025-01-07 16:25:15 +0100

    Update Daily Report.py

--- scripts/Daily Report.py
+++ scripts/Daily Report.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import streamlit as st
 
 # Parámetros
-def main(files, new_excel=None, month, year):
+def main(files, month, year, new_excel=None):
     try:
         # Verificar tipos antes de procesar
         for key, file in files.items():
