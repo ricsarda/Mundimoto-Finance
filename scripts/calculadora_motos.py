e08032e8fd5bceec20b296f3a9a8a8064ca1c3e5
Ricardo Sarda <145287890+ricsarda@users.noreply.github.com>
2025-01-15 17:48:53 +0100

    Update calculadora_motos.py

--- scripts/calculadora_motos.py
+++ scripts/calculadora_motos.py
@@ -4,7 +4,7 @@ import numpy as np
 # Cargar los datos desde un archivo CSV
 def load_data(csv_path="Motos para calcular.csv"):
     try:
-        data = pd.read_csv(csv_path, delimiter=';', encoding='utf-8')
+        data = pd.read_csv(csv_path, delimiter=',', encoding='utf-8')
         return data
     except Exception as e:
         raise RuntimeError(f"Error al cargar el archivo CSV: {str(e)}")
