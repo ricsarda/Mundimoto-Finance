91ab1246d0d5d92fa77c6ee9ce0cc6db30633763
Ricardo Sarda <145287890+ricsarda@users.noreply.github.com>
2025-09-04 15:36:24 +0200

    Update App_online_con_streamlit.py

--- App_online_con_streamlit.py
+++ App_online_con_streamlit.py
@@ -464,8 +464,8 @@ elif script_option == "DNI y Matrícula":
 elif script_option == "Revisión Pricing Web":
     st.header("Archivos necesarios")
     st.warning("Solamente se sumarán los días que llevemos este mes al LEAD TIME. "
-               "Si el cálculo fue anterior no se tendrá en cuenta.")
-    st.info("Se tienen en cuenta las motos con precio de compra")
+               "Si el cálculo es anterior no se tendrá en cuenta.")
+    st.info("Aparecerán únicamente motos con precio de compra")
     uploaded_retool = st.file_uploader("Retool CSV (table-data*.csv)", type=["csv"])
     uploaded_leadtime = st.file_uploader("Leadtime Excel", type=["xls", "xlsx"])
 
@@ -531,6 +531,7 @@ elif script_option == "Sales":
             file_name=f"Sales_{fecha}.zip",
             mime="application/zip"
         )
+
 
 
 
